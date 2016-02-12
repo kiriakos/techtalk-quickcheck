8c742a453d0d3a1c18d149de60c415d8262d46f0
Kiriakos Krastillis <kiriakos@glanzkinder.de>
2016-02-12 16:05:44 +0100

    Presentation ready

--- steps.sh
+++ steps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-for i in step{1,2,3,4}
+for i in step{1,2,4,3,5}
 do
     hs=$i.hs
+    code=$i.code
     commands="`cat $hs`"
-
+    clear
+    echo "Code:"
+    cat $code
+    echo
+    read next
+    echo "Tests"
     echo "$commands"
+    echo
     read next
     echo -e ":load scratchpad\n$commands" | ghci
+    read next
 
 done
 #:load scratchpad
